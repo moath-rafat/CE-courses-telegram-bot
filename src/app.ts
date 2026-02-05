814d4060e1b0a2738aa36184ee7a5abba97dbd44
Moath <moathrafat10@gmail.com>
2026-02-05 20:16:50 +0300

    Made the server ready for deploymet

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,11 @@ dotenv.config();
 export const TOKEN = process.env.BOT_TOKEN;
 export const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
 
+const PORT = process.env.PORT || 8000;
+
 app.use(express.json());
 app.use(cors());
 
 app.use(router);
 
-app.listen(8000);
+app.listen(PORT);
--- src/routes.ts
+++ src/routes.ts
@@ -5,4 +5,8 @@ const router = express.Router();
 
 router.post("/webhook", getMessageFromTelegram);
 
+router.get("/health", (req, res) => {
+    res.status(200).send("Bot is alive");
+});
+
 export default router;
